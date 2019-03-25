fad4afde5835133a45eaee9d69b1bb9a3982b731
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-24 23:15:19 -0700

    Add 3 instance variables and constructor

--- Product.java
+++ Product.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class Product {
+public abstract class Product {
+    private final String name;
+    private final int productID;
+    private double price;
+    public Product(String name,int productID,double price){
+        this.name=name;
+        this.productID=productID;
+        this.price=price;
+    }
 }
