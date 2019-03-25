6d70898cc8d1d4b48075a5bb47094a5337a03ae8
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-25 12:18:14 -0700

    Add get ansd set methods to ReadingStuff Class

--- Readingstuff.java
+++ Readingstuff.java
@@ -8,4 +8,20 @@ public class Readingstuff extends Product {
         this.pageNumber=pageNumber;
         this.publisher=publisher;
     }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 }
