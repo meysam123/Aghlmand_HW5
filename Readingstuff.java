88a2c2ad513f905424ec15bbb00e588d926a8811
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-25 12:13:20 -0700

    Add constructors to ReadinStuf Class

--- Readingstuff.java
+++ Readingstuff.java
@@ -1,3 +1,11 @@
 public class Readingstuff extends Product {
-    
+    private int pageNumber;
+    private String publisher;
+    public Readingstuff(){
+
+    }
+    public Readingstuff(int pageNumber,String publisher){
+        this.pageNumber=pageNumber;
+        this.publisher=publisher;
+    }
 }
