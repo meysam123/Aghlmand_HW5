8cf073dcfc00c8da9ec866aee6db4dc97c9fcab0
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-25 11:34:13 -0700

    Add constructors to Electronic Class

--- Electronics.java
+++ Electronics.java
@@ -2,5 +2,17 @@ public abstract class Electronics extends Product {
     private Color color;
     private Brand brand;
     private double weight;
+    public Electronics(){
+
+    }
+    public Electronics(Color color,Brand brand,double weight,String name,int productID,double price){
+        super(name,productID,price);
+        this.color=color;
+        this.brand=brand;
+        this.weight=weight;
+    }
+    public Electronics(Brand brand){
+        this.brand=brand;
+    }
 
 }
