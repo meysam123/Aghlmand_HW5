5cc3f5e800cd4e4463e4a473ad963a646dfe7449
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-25 00:42:28 -0700

    Create instance variables and Constructors

--- Address.java
+++ Address.java
@@ -1,2 +1,19 @@
 public class Address {
+    private String province;
+    private String city;
+    private String street;
+    private long postalCode;
+    public Address(){
+
+    }
+    public Address(String city,long postalCode){
+        this.city=city;
+        this.postalCode=postalCode;
+    }
+    public Address(String province,String city,String street,long postalCode){
+        this.province=province;
+        this.city=city;
+        this.street=street;
+        this.postalCode=postalCode;
+    }
 }
