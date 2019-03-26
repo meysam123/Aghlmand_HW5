1721412b4989bf356f133748ef9a82f76b6f69cb
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-26 00:23:51 -0700

    Add instance variables and constructors to CellPhone Class

--- CellPhone.java
+++ CellPhone.java
@@ -1,4 +1,27 @@
 public class CellPhone extends Electronics {
+    private OperatingSystem operatingSystem;
+    private ScreenType screenType;
+    private int memory;
+    private int ram;
+    private int cameraPixelSize;
+    public CellPhone(){
 
+    }
+    public CellPhone(OperatingSystem operatingSystem,ScreenType screenType,int memory,int ram,int cameraPixelSize){
+        this.operatingSystem=operatingSystem;
+        this.screenType=screenType;
+        this.memory=memory;
+        this.ram=ram;
+        this.cameraPixelSize=cameraPixelSize;
+    }
+    public CellPhone(Brand brand,int size,OperatingSystem operatingSystem,ScreenType screenType,int memory,int ram,int cameraPixelSize){
+        super(brand,size);
+        this.operatingSystem=operatingSystem;
+        this.screenType=screenType;
+        this.memory=memory;
+        this.ram=ram;
+        this.cameraPixelSize=cameraPixelSize;
+    }
+    
 
 }
