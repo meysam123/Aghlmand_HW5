efdb4c07c023ac0f88ee6cca679635d2f67001a4
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-25 22:55:55 -0700

    Add instance variables,constructors and set and get methods to Television class

--- Television.java
+++ Television.java
@@ -1,3 +1,44 @@
 public class Television extends Electronics {
+    private ScreenQuality screenQuality;
+    private ScreenTechnology screenTechnology;
+    private int size;
+    private int power;
+    public Television(){
 
+    }
+    public Television(Brand brand,ScreenQuality screenQuality,ScreenTechnology screenTechnology,int size,int power){
+        super(brand);
+        this.screenQuality=screenQuality;
+        this.screenTechnology=screenTechnology;
+        this.size=size;
+        this.power=power;
+    }
+
+    public ScreenQuality getScreenQuality() {
+        return screenQuality;
+    }
+
+    public ScreenTechnology getScreenTechnology() {
+        return screenTechnology;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setScreenQuality(ScreenQuality screenQuality) {
+        this.screenQuality = screenQuality;
+    }
+
+    public void setScreenTechnology(ScreenTechnology screenTechnology) {
+        this.screenTechnology = screenTechnology;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
